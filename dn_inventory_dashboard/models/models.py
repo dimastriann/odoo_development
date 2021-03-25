0763006eb341877dbd839f3d1bf2cdbc1e43482a
dimastriann <dimastriannugraha@gmail.com>
2021-03-25 16:59:13 +0700

    [ADD] get real data from stock picking

--- dn_inventory_dashboard/models/models.py
+++ dn_inventory_dashboard/models/models.py
@@ -1,76 +1,118 @@
 # -*- coding: utf-8 -*-
 
 import time
-from odoo import models, fields, api
+import json
+
+from odoo import models, api
 from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
 
 
 class StockPickingType(models.Model):
     _inherit = 'stock.picking.type'
 
-    @api.model
+    # @api.model
     def get_stock_dashboard_data(self):
-        # server_date = time.strftime(DEFAULT_SERVER_DATETIME_FORMAT)
-        # domains = {
-        #     # 'count_picking_draft': "sp.state = 'draft'",
-        #     'count_picking_waiting': "sp.state in ('confirmed', 'waiting')",
-        #     'count_picking_ready': "sp.state = 'assigned'",
-        #     # 'count_picking': "sp.state in ('assigned', 'waiting', 'confirmed')",
-        #     'count_picking_late': "sp.scheduled_date < '%s' AND sp.state in ('assigned', 'waiting', 'confirmed')" % server_date,
-        #     'count_picking_backorders': "sp.backorder_id NOTNULL AND sp.state in ('confirmed', 'assigned', 'waiting')",
-        # }
-        #
-        # results = []
-        # for pick_code in ['incoming', 'internal', 'outgoing']:
-        #     query = """SELECT count(sp.id) as picking_count FROM stock_picking sp
-        #                 LEFT JOIN stock_picking_type spt on spt.id = sp.picking_type_id"""
-        #     query_reset = query
-        #     receipts, internal, delivery = [], [], []
-        #     for key, value in domains.items():
-        #         if pick_code == 'incoming':
-        #             query_reset += " WHERE " + value + " AND spt.code = 'incoming'"
-        #             self._cr.execute(query_reset)
-        #             receipts.append({key + "_in":self._cr.dictfetchall()[0].get('picking_count')})
-        #             query_reset = query
-        #         elif pick_code == 'internal':
-        #             query_reset += " WHERE " + value + " AND spt.code = 'internal'"
-        #             self._cr.execute(query_reset)
-        #             internal.append({key + "_int":self._cr.dictfetchall()[0].get('picking_count')})
-        #             query_reset = query
-        #         elif pick_code == 'outgoing':
-        #             query_reset += " WHERE " + value + " AND spt.code = 'outgoing'"
-        #             self._cr.execute(query_reset)
-        #             delivery.append({key + "_out":self._cr.dictfetchall()[0].get('picking_count')})
-        #             query_reset = query
-        #     if pick_code == 'incoming':
-        #         results.append(receipts)
-        #     if pick_code == 'internal':
-        #         results.append(internal)
-        #     if pick_code == 'outgoing':
-        #         results.append(delivery)
+        domains = {
+            # 'draft': [('state', '=', 'draft')],
+            'waiting': [('state', 'in', ('confirmed', 'waiting'))],
+            'ready': [('state', '=', 'assigned')],
+            'late': [('scheduled_date', '<', time.strftime(DEFAULT_SERVER_DATETIME_FORMAT)),
+                     ('state', 'in', ('assigned', 'waiting', 'confirmed'))],
+            'backorder': [('backorder_id', '!=', False), ('state', 'in', ('confirmed', 'assigned', 'waiting'))],
+        }
 
+        context = {
+            'search_default_picking_type_code': '%s',
+            'search_default_%s': True,
+            'search_default_group_by_picking_type': True
+        }
+
+        stock_data = []
+        for operation in sorted(set(self.mapped('code'))):
+            op_type = "Receipts" if operation == 'incoming' else ("Delivery" if operation == 'outgoing' else operation)
+            results = {}
+            for field in domains:
+                data = self.env['stock.picking'].search_read(
+                    domains[field] + [('state', 'not in', ('done', 'cancel')), ('picking_type_id', 'in', self.ids),
+                                      ('picking_type_code', '=', operation)], ['picking_type_code'])
+                state = "available" if field == 'ready' else field
+                res_data = {
+                    'val': len(data),
+                    'domain': json.dumps([d.get('id') for d in data]),
+                    'context': str(context) % (operation, state),
+                    'name': "All %s %s" % (op_type.capitalize(), field.capitalize())
+                }
+                results.update({field: res_data})
+            results.update({
+                'name': 'All %s' % op_type.capitalize(),
+                'context': str({"search_default_picking_type_code": operation})
+            })
+            stock_data.append(results)
+
+        return stock_data
+        # return self.test_data_stock()
+
+    # stock data test #
+    def test_data_stock(self):
         receipts = {
             'name': 'All Receipts',
-            'data': {'ready': 5, 'waiting': 5,
-                     'context': '{"search_default_picking_type_code": "incoming", "search_default_available": True}'},
             'context': '{"search_default_picking_type_code": "incoming"}',
+            'ready': {
+                'val': 5, 'name': 'All Receipts Ready', 'domain': json.dumps([]),
+                'context': '{"search_default_picking_type_code": "incoming", "search_default_available": True}'
+            },
+            'waiting': {
+                'val': 5, 'name': 'All Receipts Waiting', 'domain': json.dumps([]),
+                'context': '{"search_default_picking_type_code": "incoming", "search_default_waiting": True}'
+            },
+            'late': {
+                'val': 5, 'name': 'All Receipts Late', 'domain': json.dumps([]),
+                'context': '{"search_default_picking_type_code": "incoming", "search_default_late": True}'
+            },
+            'backorder': {
+                'val': 5, 'name': 'All Receipts Backorders', 'domain': json.dumps([]),
+                'context': '{"search_default_picking_type_code": "incoming", "search_default_backorder": True}'
+            },
         }
+
         internal = {
-            'name': 'All Internal',
-            'data': {'ready': 6, 'waiting': 6,
-                     'context': '{"search_default_picking_type_code": "internal", "search_default_available": True}'},
+            'name': 'All Internal Transfer',
             'context': '{"search_default_picking_type_code": "internal"}',
+            'ready': {
+                'val': 5, 'name': 'All Internal Ready', 'domain': json.dumps([]),
+                'context': '{"search_default_picking_type_code": "internal", "search_default_available": True}'
+            },
+            'waiting': {
+                'val': 5, 'name': 'All Internal Waiting', 'domain': json.dumps([]),
+                'context': '{"search_default_picking_type_code": "internal", "search_default_waiting": True}'
+            },
+            'late': {
+                'val': 5, 'name': 'All Internal Late', 'domain': json.dumps([]),
+                'context': '{"search_default_picking_type_code": "internal", "search_default_late": True}'
+            },
+            'backorder': {
+                'val': 5, 'name': 'All Internal Backorders', 'domain': json.dumps([]),
+                'context': '{"search_default_picking_type_code": "internal", "search_default_backorder": True}'
+            },
         }
         delivery = {
             'name': 'All Delivery',
-            'data': {'ready': 7, 'waiting': 7,
-                     'context': '{"search_default_picking_type_code": "incoming", "search_default_available": True}'},
             'context': '{"search_default_picking_type_code": "outgoing"}',
+            'ready': {
+                'val': 5, 'name': 'All Delivery Ready', 'domain': json.dumps([]),
+                'context': '{"search_default_picking_type_code": "outgoing", "search_default_available": True}'
+            },
+            'waiting': {
+                'val': 5, 'name': 'All Delivery Waiting', 'domain': json.dumps([]),
+                'context': '{"search_default_picking_type_code": "outgoing", "search_default_waiting": True}'
+            },
+            'late': {
+                'val': 5, 'name': 'All Delivery Late', 'domain': json.dumps([]),
+                'context': '{"search_default_picking_type_code": "outgoing", "search_default_late": True}'
+            },
+            'backorder': {
+                'val': 5, 'name': 'All Delivery Backorders', 'domain': json.dumps([]),
+                'context': '{"search_default_picking_type_code": "outgoing", "search_default_backorder": True}'
+            },
         }
-        results = {
-            'incoming': receipts,
-            'internal': internal,
-            'outgoing': delivery,
-        }
-        # results = [receipts, internal, delivery]
-        return results
+        return receipts, internal, delivery
--- dn_inventory_dashboard/static/src/js/inventory_dashboard.js
+++ dn_inventory_dashboard/static/src/js/inventory_dashboard.js
@@ -15,54 +15,6 @@ var QWeb = core.qweb;
 // Kanban View
 //--------------------------------------------------------------------------
 
-var StockKanbanDashboardModel = KanbanModel.extend({
-    /**
-     * @override
-     */
-    init: function() {
-        this.stockDashboardValues = {};
-        this._super.apply(this, arguments);
-    },
-    /**
-     * @override
-     */
-    __get: function (localID) {
-        var result = this._super.apply(this, arguments);
-        if (_.isObject(result)) {
-            result.stockDashboardValues = this.stockDashboardValues[localID];
-        }
-        return result;
-    },
-    /**
-     * @override
-     * @returns {Promise}
-     */
-    __load: function() {
-        return this._loadDashboard(this._super.apply(this, arguments));
-    },
-    /**
-     * @override
-     * @returns {Promise}
-     */
-    __reload: function() {
-        return this._loadDashboard(this._super.apply(this, arguments));
-    },
-
-    _loadDashboard: function(super_def) {
-        var self = this;
-        var dashboard_stock_def = this._rpc({
-            model: "stock.picking.type",
-            method: "get_stock_dashboard_data",
-        });
-        return Promise.all([super_def, dashboard_stock_def]).then(function(results){
-            var id = results[0];
-            var stockDashboardValues = results[1];
-            self.stockDashboardValues[id] = stockDashboardValues;
-            return id;
-        });
-    },
-});
-
 var StockKanbanDashboardController = KanbanController.extend({
     custom_events:_.extend({}, KanbanController.prototype.custom_events, {
         dashboard_open_action: '_onDashboardOpenAction',
@@ -73,16 +25,18 @@ var StockKanbanDashboardController = KanbanController.extend({
      * @param {OdooEvent} event
      */
     _onDashboardOpenAction: function(event) {
-//        return this.do_action(event.data.action_name,
-//            {additional_context: JSON.parse(event.data.action_context)});
-        return this.do_action({
+        let action_act_window = {
             name: event.data.action_name,
             res_model: "stock.picking",
             context: event.data.action_context,
             views: [[false, 'list'], [false, 'form']],
             type: 'ir.actions.act_window',
-            view_mode: "list"
-        });
+            view_mode: "list",
+        }
+        if(event.data.domain){
+            action_act_window['domain'] = [['id', 'in', event.data.domain]]
+        }
+        return this.do_action(action_act_window);
     },
 });
 
@@ -91,15 +45,17 @@ var StockKanbanDashboardRenderer = KanbanRenderer.extend({
         'click .o_dn_inventory_action': '_onDashboardOpenClicked',
     }),
 
-    _render: function () {
+    _render: async function () {
         var self = this;
-        return this._super.apply(this, arguments).then(function () {
-            var values = self.state.stockDashboardValues;
-            console.log('values', values);
-            var title_ex = 'Example Title';
-            var stock_dashboard = QWeb.render('dn.InventoryDashboard', {
-                stockValues: values,
-                title_test: title_ex,
+        const super_def = await this._super(...arguments);
+        const stockValues = await this._rpc({
+            model: "stock.picking.type",
+            method: "get_stock_dashboard_data",
+            args:[this.state.res_ids],
+        });
+        return Promise.all([super_def, stockValues]).then(function(results){
+            const stock_dashboard = QWeb.render('dn.InventoryDashboard', {
+                stockValues: results[1],
             });
             self.$el.prepend(stock_dashboard);
         });
@@ -111,6 +67,7 @@ var StockKanbanDashboardRenderer = KanbanRenderer.extend({
         this.trigger_up('dashboard_open_action', {
             action_name: $action.attr('title'),
             action_context: $action.attr('context'),
+            domain: $action.data('domain'),
         });
     },
 });
@@ -118,7 +75,6 @@ var StockKanbanDashboardRenderer = KanbanRenderer.extend({
 //
 var StockKanbanDashboardView = KanbanView.extend({
     config: _.extend({}, KanbanView.prototype.config, {
-        Model: StockKanbanDashboardModel,
         Renderer: StockKanbanDashboardRenderer,
         Controller: StockKanbanDashboardController,
     })
@@ -127,10 +83,9 @@ var StockKanbanDashboardView = KanbanView.extend({
 // add custom stock picking type kanban dashboard to view registry
 view_registry.add('stock_picking_type_kanban_dashboard', StockKanbanDashboardView);
 
-//return {
-////    StockKanbanDashboardModel: StockKanbanDashboardModel,
-//    StockKanbanDashboardRenderer: StockKanbanDashboardRenderer,
-//    StockKanbanDashboardController: StockKanbanDashboardController,
-//};
+return {
+    StockKanbanDashboardRenderer,
+    StockKanbanDashboardController,
+};
 
 });
